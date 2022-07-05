7dcc7a4e8da111448dd00e8d27c0a69f7ad15ce5
Emmanuel Manishimwe <emanishimwe@eucl.reg.rw>
2022-07-05 11:31:49 +0200

    first version for music player app

---
+++ lib/neu_box.dart
@@ -0,0 +1,34 @@
+import "package:flutter/material.dart";
+
+class NeuBox extends StatelessWidget {
+  final child;
+  const NeuBox({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Center(
+        child: child,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            // dark shadow on the bottom right
+            BoxShadow(
+              color: Colors.grey.shade500,
+              blurRadius: 15,
+              offset: Offset(5, 5),
+            ),
+
+            // lighter shadow in the top left
+            const BoxShadow(
+              color: Colors.white,
+              blurRadius: 15,
+              offset: Offset(-5, -5),
+            )
+          ]),
+    );
+  }
+}
